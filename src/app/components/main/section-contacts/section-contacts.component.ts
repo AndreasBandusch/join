bb24fb2ca1237c0722e50c097c9ed0f699bcd11c
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-02-03 10:45:20 +0100

    Added updates

--- src/app/components/main/section-contacts/section-contacts.component.ts
+++ src/app/components/main/section-contacts/section-contacts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-// import { db } from '../../../database';
+
 
 @Component({
   selector: 'app-section-contacts',
