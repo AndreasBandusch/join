9038a364e00fd27a4c2acb94d2e55843bdc6bdc4
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-02-21 11:14:58 +0100

    Added updates

--- src/app/components/main/section-add-task/section-add-task.component.ts
+++ src/app/components/main/section-add-task/section-add-task.component.ts
@@ -25,6 +25,7 @@ export class SectionAddTaskComponent implements OnInit, OnDestroy {
   placeholder: string = 'New category name';
   categoryName: string = '';
   catColors: string[] = ['#8fa6fc', '#e83400', '#6bce33', '#ee8f11', '#cd37b9', '#0e45fa'];
+  currentCatColor: string = '';
  
 
   @HostListener('window:resize')
