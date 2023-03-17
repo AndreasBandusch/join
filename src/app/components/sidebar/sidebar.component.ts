d2e5e20be56daa79d44aa1b2a5fc8b9ca4bfbf11
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-03-17 11:14:08 +0100

    Added updates

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,21 +8,54 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidebar.component.scss']
 })
 
+export class SidebarComponent implements OnInit {
+  isSummaryActive: boolean = true;
+  isBoardActive: boolean = false;
+  isAddTaskActive: boolean = false;
+  isContactsActive: boolean = false;
+  isImpressActive: boolean = false;
+  isPrivacyActive: boolean = false;
+  activeStart: string = '';
 
-export class SidebarComponent {
-  constructor(private router: Router) { }
-
-  isSummaryActive = true;
-  isBoardActive = false;
-  isAddTaskActive = false;
-  isContactsActive = false;
-  isImpressActive = false;
-  isPrivacyActive = false
-
-  navigate() {
-    this.router.navigate(['/login']);
+  constructor(
+    private router: Router,
+    private location: Location) {
   }
 
+
+  ngOnInit(): void {
+    this.activeStart = this.location.path();
+    this.setActiveStart();
+  }
+
+
+  // Sets the active start link
+  setActiveStart() {
+    switch (true) {
+      case this.activeStart.includes('summary'):
+        this.setActive('summary');
+        break;
+      case this.activeStart.includes('board'):
+        this.setActive('board');
+        break;
+      case this.activeStart.includes('add-task'):
+        this.setActive('addTask');
+        break;
+      case this.activeStart.includes('contact-list'):
+        this.setActive('contacts');
+        break;
+      case this.activeStart.includes('impress'):
+        this.setActive('impress');
+        break;
+      default:
+        this.setActive('privacy');
+        break;
+    }
+  }
+
+  // navigate() {
+  //   this.router.navigate(['/login']);
+  // }
 
   // Sets the active link
   setActive(clickedLink: string): void {
