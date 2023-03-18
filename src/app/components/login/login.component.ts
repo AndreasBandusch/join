bcb6254230cd6e28aac86b936a5e066193f1ffae
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-03-18 13:01:24 +0100

    Added updates

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  noUserFound = false;
+  wrongPassword = false;
+                       ;
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
+
+userLogin() {
+  this.noUserFound = false;
+  this.wrongPassword = false;
+
+  this.auth
+      .signInWithEmailAndPassword(this.email, this.password)
+      .then(res => {
+          this.router.navigate(['/kanban']);
+          console.log(res.user?.displayName + ' erfolgreich eingeloggt ', res);
+      })
+      .catch(error => {
+         if (error.message.includes('no user record corresponding')) {
+          this.noUserFound = true; 
+         } 
+
+         else if (error.message.includes('password is invalid')) {
+            this.wrongPassword = true;
+         }
+      });
+}
+
+
+guestLogin() {
+  this.auth.signInAnonymously()
+  .then(() => {
+    console.log('Hello Guest!')
+    this.router.navigate(['/kanban']);
+  }).catch(err => {
+    console.log(err.message);
+    
+  })
+}
+ 
 }
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+userName: string = '';
+email: string = '';
+password: string = '';
 
+
+
+
+constructor(private auth: AngularFireAuth, private router: Router){}
+
+         
+  signUp() {
+    this.auth
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(res => {
+             if (res.user) {           
+                 res.user.updateProfile({ displayName: this.userName });
+                 this.router.navigate(['/kanban/summary']);
+             }
+            console.log('User registriert');
+        })
+        .catch(error => {
+            console.log('Something is wrong:', error.message);
+        });
+}
 }
